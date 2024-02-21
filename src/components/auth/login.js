0020f64b8f015043daaaf543cef4f3d17f9e182d
Uthaeus <romanlavery@gmail.com>
2024-02-21 10:59:10 -0700

    installed react hook form, built simple forms for login and register

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Login() {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
             <h1>Login</h1>
-            <Link to="/register">Register</Link>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" placeholder="Email" {...register("email")} />
+                <input type="password" placeholder="Password" {...register("password")} />
+                <input type="submit" />
+            </form>
+            <Link to="/auth/register">Register</Link>
         </div>
     );
 };
--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Register() {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        if (data.password !== data.confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+        console.log(data);
+    };
+
     return (
         <div>
             <h1>Register</h1>
-            <Link to="/login">Login</Link>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" placeholder="Email" {...register("email")} />
+                <input type="password" placeholder="Password" {...register("password")} />
+                <input type="password" placeholder="Confirm Password" {...register("confirmPassword")} />
+                <input type="submit" />
+            </form>
+            <Link to="/auth/login">Login</Link>
         </div>
     );
 };
