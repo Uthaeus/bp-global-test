4b1eaaa31736591636162d3dbc0b6c3c681ab6b4
Uthaeus <romanlavery@gmail.com>
2024-02-23 11:38:54 -0700

    moved user context call to sidebar, added logout functionality to account sidebar

--- src/components/account-layout.js
+++ src/components/account-layout.js
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router";
-import { useContext } from "react";
-
-import { UserContext } from "../store/user-context";
 
 import MainNavigation from "./navigation/main-navigation";
 import AccountSidebar from "./account/account-sidebar";
 
 function AccountLayout() {
-
-    const { user } = useContext(UserContext);
 
     return (
         <div className="account-layout">
--- src/components/account/account-sidebar.js
+++ src/components/account/account-sidebar.js
@@ -1,13 +1,20 @@
 import { NavLink, Link } from "react-router-dom";
+import { useContext } from "react";
+import { signOut } from "firebase/auth";
 
-function AccountSidebar({ user }) {
+import { auth } from "../../firebase";
+import { UserContext } from "../../store/user-context";
+
+function AccountSidebar() {
+    const { currentUser: user } = useContext(UserContext);
+
     return (
         <div className="account-sidebar">
-            <h2>Hello {user.name}</h2>
-            <NavLink to='/account' className={({ isActive }) => isActive ? 'account-sidebar-link active' : 'account-sidebar-link'}>Account Home</NavLink>
-            <NavLink to='/account/orders' className={({ isActive }) => isActive ? 'account-sidebar-link active' : 'account-sidebar-link'}>Orders</NavLink>
-            <NavLink to='/account/profile' className={({ isActive }) => isActive ? 'account-sidebar-link active' : 'account-sidebar-link'}>Profile</NavLink>
-            <Link to='/' className="account-sidebar-link">Logout</Link>
+            <h2>Hello {user.email}</h2>
+            <NavLink to='/account' className={({ isActive }) => isActive ? 'account-sidebar-link sidebar-link-active' : 'account-sidebar-link'}>Account Home</NavLink>
+            <NavLink to='/account/orders' className={({ isActive }) => isActive ? 'account-sidebar-link account-sidebar-link-active' : 'account-sidebar-link'}>Orders</NavLink>
+            <NavLink to='/account/profile' className={({ isActive }) => isActive ? 'account-sidebar-link account-sidebar-link-active' : 'account-sidebar-link'}>Profile</NavLink>
+            <Link to='/' className="account-sidebar-link" onClick={() => signOut(auth)}>Logout</Link>
         </div>
     );
 }
