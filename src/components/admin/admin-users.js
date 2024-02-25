8bfa81535d18fbcf0c7da3c33a9754d3d8e90cfa
Uthaeus <romanlavery@gmail.com>
2024-02-25 12:09:10 -0700

    populating admin and users to display in admin-users page

--- src/components/admin/admin-users.js
+++ src/components/admin/admin-users.js
@@ -1,27 +1,40 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import { collection } from "firebase/firestore";
+import { collection, query, onSnapshot } from "firebase/firestore";
 
 import { db } from "../../firebase";
 
 function AdminUsers() {
 
     const [users, setUsers] = useState([]);
+    const [adminUsers, setAdminUsers] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    // useEffect(() => {
-    //     const usersCollection = collection(db, 'users');
-    //     usersCollection.get().then((querySnapshot) => {
-    //         const usersData = querySnapshot.docs.map((doc) => ({
-    //             id: doc.id,
-    //             ...doc.data()
-    //         }));
-    //     });
+    useEffect(() => {
+        const usersRef = collection(db, 'users');
+        const q = query(usersRef);
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const u = [];
+            const a = [];
+            querySnapshot.forEach((doc) => {
+                if (doc.data().role === 'user') {
+                    u.push({ ...doc.data(), id: doc.id });
+                } else if (doc.data().role === 'admin') {
+                    a.push({ ...doc.data(), id: doc.id });
+                }
+            });
+            setUsers(u);
+            setAdminUsers(a);
+        });
 
-    //     setUsers(usersData);
-    //     setLoading(false);
-    // }, []);
+        setLoading(false);
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+    
 
     if (loading) {
         return <div>Loading...</div>;
@@ -29,6 +42,15 @@ function AdminUsers() {
 
     return (
         <div className="admin-users-container">
+            <h1>Admin</h1>
+            <ul>
+                {adminUsers.map((user) => (
+                    <li key={user.id}>
+                        <Link to={`/admin/users/${user.id}`}>{user.email}</Link>
+                    </li>
+                ))}
+            </ul>
+
             <h1>Users</h1>
             <ul>
                 {users.map((user) => (
