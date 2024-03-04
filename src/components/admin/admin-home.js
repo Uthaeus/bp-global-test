d17325f44c548714d344dafe3343590d7510efae
Uthaeus <romanlavery@gmail.com>
2024-03-04 12:19:27 -0700

    created useEffect for setting orders and users in admin home

--- src/components/admin/admin-home.js
+++ src/components/admin/admin-home.js
@@ -1,7 +1,33 @@
+import { useState, useEffect, useContext } from "react";
+import { collection, getDocs, doc } from "firebase/firestore";
 
+import { db } from "../../firebase";
+import { UserContext } from "../../store/user-context";
 
 function AdminHome() {
+    const [orders, setOrders] = useState([]);
+    const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+        async function getOrders() {
+            const ordersRef = collection(db, 'orders');
+            const querySnapshot = await getDocs(ordersRef);
+            const orders = querySnapshot.docs.map(doc => doc.data());
+            setOrders(orders);
+        }
+        getOrders();
+    }, []);
+
+    useEffect(() => {
+        async function getUsers() {
+            const usersRef = collection(db, 'users');
+            const querySnapshot = await getDocs(usersRef);
+            const users = querySnapshot.docs.map(doc => doc.data());
+            setUsers(users);
+        }
+        getUsers();
+    }, []);
+    
     return (
         <div>
             <h1>Admin Home</h1>
