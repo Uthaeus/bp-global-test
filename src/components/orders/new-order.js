e9b2e3d2d5cc6f8554bb3252ec28c9da96cddd3a
Uthaeus <romanlavery@gmail.com>
2024-03-05 14:35:31 -0700

    built out full new order form

--- src/components/orders/new-order.js
+++ src/components/orders/new-order.js
@@ -35,7 +35,7 @@ function NewOrder() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="row">
                     <div className="col-md-6">
-                        <div className="form-group">
+                        <div className="form-group mb-3">
                             <label htmlFor="user">User</label>
                             <select {...register("user")} className="form-control">
                                 <option value="">Select User</option>
@@ -46,25 +46,73 @@ function NewOrder() {
                                 ))}
                             </select>
                         </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="po-number">PO Number</label>
+                            <input type="text" {...register("po-number")} className="form-control" />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="order-number">Order Number</label>
+                            <input type="text" {...register("order-number")} className="form-control" />
+                        </div>
                     </div>
 
                     <div className="col-md-6">
-                        
+                        <div className="form-group mb-3">
+                            <label htmlFor="ordered-date">Ordered Date</label>
+                            <input type="date" {...register("ordered-date")} className="form-control" />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="delivery-date">Est. Delivery Date</label>
+                            <input type="date" {...register("delivery-date")} className="form-control" />
+                        </div>
                     </div>
                 </div>
 
                 <div className="row">
                     <div className="col-md-6">
-                        <div className="form-group">
+                        <div className="form-group mb-3">
                             <label htmlFor="product">Product</label>
                             <input type="text" {...register("product")} className="form-control" />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="destination">Destination</label>
+                            <input type="text" {...register("destination")} className="form-control" />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="carrier">Carrier</label>
+                            <input type="text" {...register("carrier")} className="form-control" />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="origin">Origin</label>
+                            <input type="text" {...register("origin")} className="form-control" />
                         </div>
                     </div>
 
                     <div className="col-md-6">
-                        <div className="form-group">
+                        <div className="form-group mb-3">
                             <label htmlFor="quantity">Quantity</label>
                             <input type="number" {...register("quantity")} className="form-control" />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="order-status">Order Status</label>
+                            <input type="text" {...register("order-status")} className="form-control" />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="ship-date">Ship Date</label>
+                            <input type="date" {...register("ship-date")} className="form-control" />
+                        </div>
+
+                        <div className="form-group mb-3">
+                            <label htmlFor="last-location">Last Location</label>
+                            <input type="text" {...register("last-location")} className="form-control" />
                         </div>
                     </div>
                 </div>
